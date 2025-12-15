b26877070368e82aa83b31533d9013f52720e86d
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-12-15 18:59:14 +0100

    cambios

--- docker/proyecto_react/dockerfiles/ubbase/admin/ssh/mainssh.sh
+++ docker/proyecto_react/dockerfiles/ubbase/admin/ssh/mainssh.sh
@@ -7,6 +7,10 @@ configurar_ssh() {
   # Cambiar el puerto de SSH
   sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
 
+  sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/ssh_config
+  # Cambiar el puerto de SSH
+  sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/ssh_config
+
   mkdir -p /run/sshd
   mkdir /home/${USUARIO}/.ssh
   cat /root/admin/ubbase/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys

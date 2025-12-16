de962717b430aa541abbb236be73c9d8e0280b09
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-12-16 13:04:50 +0100

    Primera Prueba

---
+++ docker/proyecto_react/dockerfiles/ubsecurity/admin/start.sh
@@ -0,0 +1,14 @@
+set -e
+
+source /root/admin/ubbase/usuarios/mainuser.sh
+source /root/admin/ubbase/ssh/mainssh.sh
+source /root/admin/ubbase/sudo/mainsudo.sh
+
+puertos(){
+    mkdir -p /root/logs
+    registro=$(nmap -sS -p- localhost | grep open)
+    echo "Puertos abiertos en el contenedor:" > /root/logs/puertos.log
+    echo "$registro" >> /root/logs/puertos.log
+}
+
+puertos

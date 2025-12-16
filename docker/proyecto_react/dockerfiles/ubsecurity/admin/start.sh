95e0b8df88f34b3308cb7add316b902a9a926c1d
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2025-12-16 13:17:22 +0100

    Cambio ciberseguridad

--- docker/proyecto_react/dockerfiles/ubsecurity/admin/start.sh
+++ docker/proyecto_react/dockerfiles/ubsecurity/admin/start.sh
@@ -4,7 +4,15 @@ source /root/admin/ubbase/usuarios/mainuser.sh
 source /root/admin/ubbase/ssh/mainssh.sh
 source /root/admin/ubbase/sudo/mainsudo.sh
 
-puertos(){
+puertos
+
+
+
+nginx &
+
+tail -f /dev/null
+
+(){
     mkdir -p /root/logs
     registro=$(nmap -sS -p- localhost | grep open)
     echo "Puertos abiertos en el contenedor:" > /root/logs/puertos.log

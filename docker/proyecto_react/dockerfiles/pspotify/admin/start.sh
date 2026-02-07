e62dd02f00f5082b8baff17e85f58491fa992f06
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2026-02-07 16:53:41 +0100

    cambios spotifyç

--- docker/proyecto_react/dockerfiles/pspotify/admin/start.sh
+++ docker/proyecto_react/dockerfiles/pspotify/admin/start.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
+set -e
 
-set -e 
-
-workdir(){
-    cd /root/admin/node/proyecto/pproyecto
-}
-construir_copiar(){
-    npm install
-    npm run build
-    cp -r dist/* /var/www/html/
-}
 config_nginx(){
     nginx -g 'daemon off;'
 }
+
 main(){
-    workdir
-    construir_copiar
     config_nginx
 }
 

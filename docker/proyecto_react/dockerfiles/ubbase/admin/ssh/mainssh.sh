c7cc0d2b28eb93385a0f46a88a1529122b1eb328
DiegoMoralesRidao <dmorrid282@g.educaand.es>
2026-02-25 12:25:57 +0100

    Cambio ssh

--- docker/proyecto_react/dockerfiles/ubbase/admin/ssh/mainssh.sh
+++ docker/proyecto_react/dockerfiles/ubbase/admin/ssh/mainssh.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
 configurar_ssh() {
-
   # Deshabilitar el login de root
-  sed -i 's/#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+  # Si existe comentada o no, la cambiamos a 'no'
+  if grep -q "PermitRootLogin" /etc/ssh/sshd_config; then
+    sed -i 's/.*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+  else
+    echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+  fi
+
   # Cambiar el puerto de SSH
-  sed -i 's/#Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
+  if [ -n "$PORT_SSH" ]; then
+    if grep -q "Port" /etc/ssh/sshd_config; then
+      sed -i 's/.*Port.*/Port '$PORT_SSH'/' /etc/ssh/sshd_config
+    else
+      echo "Port $PORT_SSH" >> /etc/ssh/sshd_config
+    fi
+  fi
 
   mkdir -p /run/sshd
-  mkdir /home/${USUARIO}/.ssh
-  cat /root/admin/ubbase/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
-    #/etc/init.d/ssh start
-  # Reinicar el servicio SSH para que se aplique las configuraciones
-  #service ssh restart
-  #exec /usr/sbin/sshd -D & # dejar el ssh en background PARA CUANDO LO IMPLEMENTOS EN UN SERVICIO
+  mkdir -p /home/${USUARIO}/.ssh
+  
+  if [ -f "/root/admin/ubbase/common/id_rsa.pub" ]; then
+    cat /root/admin/ubbase/common/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    chmod 600 /home/${USUARIO}/.ssh/authorized_keys
+    chown -R ${USUARIO}:${USUARIO} /home/${USUARIO}/.ssh
+  else
+    echo "Warning: Public key not found at /root/admin/ubbase/common/id_rsa.pub" >> /root/logs/informe.log
+  fi
+
   exec /usr/sbin/sshd -D &
 }
